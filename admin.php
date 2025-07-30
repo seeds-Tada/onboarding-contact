5f2959581c51029175c46f3d9a140030aabaca4f
seeds-Tada <k.tada@seeds-std.co.jp>
2025-07-30 17:07:13 +0900

    0730_お問合せ内容の一覧画面を作った

---
+++ admin.php
@@ -0,0 +1,95 @@
+<?php
+require_once 'private/bootstrap.php';
+require_once 'private/database.php';
+// 実装
+//http://localhost/admin.php
+
+$keiyuArray = [];
+$contactArray = [];
+$connection = connectDB();
+try {
+	$keiyuArray = [];
+	$sql = "SELECT * FROM keiyu;";
+	$stmt = mysqli_query($connection, $sql);
+	while($row = mysqli_fetch_assoc($stmt)) {
+		$keiyuArray[] = $row;
+	}
+	$stmt->close();
+
+	$contactArray = [];
+	$sql = "SELECT * FROM contacts;";
+	$stmt = mysqli_query($connection, $sql);
+	while($row = mysqli_fetch_assoc($stmt)) {
+		$row['keiyu'] = [];
+		foreach($keiyuArray as $keiyu) {
+			if($row['id'] === $keiyu['contacts_id']) {
+				array_push($row['keiyu'], $keiyu['keiyu']);
+			}
+		}
+		$contactArray[] = $row;
+	}
+	$stmt->close();
+}catch(PDOException $e) {
+	echo("error");
+}catch(Exception $e) {
+	echo("error");
+	echo($e);
+}
+?>
+
+<!-- 描画するHTML -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>onboarding-contact</title>
+</head>
+<body>
+	<div>
+		<h1>お問合せ内容一覧画面</h1>
+	</div>
+	<div>
+		
+		<table border="1">
+			<tr>
+				<th>氏名</th>
+				<th>フリガナ</th>
+				<th>メールアドレス</th>
+				<th>性別</th>
+				<th>郵便番号</th>
+				<th>都道府県</th>
+				<th>市区町村</th>
+				<th>それ以降</th>
+				<th>建物</th>
+				<th>お問い合わせ内容</th>
+				<th>経由</th>
+			</tr>
+			<?php
+				foreach($contactArray as $result) {
+					echo("<tr>");
+					foreach($result as $key => $val) {
+						if($key !== 'id') {
+							if($key !== 'keiyu') {
+								if($key !== 'post') {
+									echo("<td>" . $val . PHP_EOL."</td>");
+								}else {
+									echo("<td>" . substr_replace($val, "-", 3, 0) . PHP_EOL."</td>");
+								}
+							}else {
+								foreach($val as $val_keiyu){
+									echo("<td style='display: block'>".$val_keiyu."</td>");
+								}
+							}
+						}
+					}
+					echo("</tr>");
+				}
+			?>
+			<tr>
+
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
